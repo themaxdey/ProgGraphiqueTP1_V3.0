7eea653cc5e72bf36e7f444c7a13023bf56edbb9
Jsbeaulne <jeansebastien321@gmail.com>
2019-02-20 21:21:04 -0500

    Essai de zone dessin

--- src/ZoneDessin.java
+++ src/ZoneDessin.java
@@ -1,5 +1,20 @@
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ZoneDessin extends JPanel{
 	private static final long serialVersionUID = 1L;
+	
+	public ZoneDessin(Dimension dimension) {
+		setSize((int) (dimension.getWidth() * 0.95), (int) (dimension.getHeight() * 0.75));
+		setBackground(Color.WHITE);
+		setBorder(BorderFactory.createRaisedBevelBorder());
+	}
+	
+	public ZoneDessin(Dimension dimension, String nomFichier) {
+		//quand on va ouvrir un fichier
+	}
 }
